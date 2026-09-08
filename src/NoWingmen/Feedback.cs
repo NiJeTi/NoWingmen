c51f6edd9b731d4ac387ed5b9bb615cd898d5a0a
Arkady Skvoretsky <nijeti.inc@gmail.com>
2026-09-08 19:54:12 +0400

    feat(plugin): add visual notifications

--- src/NoWingmen/Feedback.cs
+++ src/NoWingmen/Feedback.cs
@@ -1,10 +1,13 @@
 using HarmonyLib;
+using NuclearOption.UIStyleSystem;
 using UnityEngine;
 
 namespace NoWingmen;
 
 internal static class Feedback
 {
+    private const float ReportSeconds = 5f;
+
     private static readonly AccessTools.FieldRef<CombatHUD, AudioClip> SelectSoundRef =
         AccessTools.FieldRefAccess<CombatHUD, AudioClip>("selectSound");
 
@@ -17,24 +20,42 @@ internal static class Feedback
     private static readonly AccessTools.FieldRef<CombatHUD, AudioClip> WeaponSwitchSoundRef =
         AccessTools.FieldRefAccess<CombatHUD, AudioClip>("weaponSwitchSound");
 
-    public static void OnWingAdd()
+    public static void OnWingAdd(Unit unit)
     {
         Play(SelectSoundRef);
+        Report($"{Name(unit)} added to wing");
     }
 
-    public static void OnWingRemove()
+    public static void OnWingRemove(Unit unit)
     {
         Play(DeselectSoundRef);
+        Report($"{Name(unit)} removed from wing");
     }
 
-    public static void OnWingReject()
+    public static void OnWingReject(string reason)
     {
         Play(DeselectAllSoundRef);
+        Report(reason.AddColor(ThemeManager.Active.ColorTheme.Alert));
     }
 
-    public static void OnLockPreventionToggle()
+    public static void OnLockPreventionToggle(bool enabled)
     {
         Play(WeaponSwitchSoundRef);
+        Report($"Lock prevention <b>{(enabled ? "ENABLED" : "DISABLED")}</b>");
+    }
+
+    private static string Name(Unit unit)
+    {
+        return $"<b>{Identity.GetDisplayName(unit).AddColor(Config.WingColor.Value)}</b>";
+    }
+
+    private static void Report(string message)
+    {
+        var report = SceneSingleton<AircraftActionsReport>.i;
+        if (report != null)
+        {
+            report.ReportText(message, ReportSeconds);
+        }
     }
 
     private static void Play(AccessTools.FieldRef<CombatHUD, AudioClip> clipRef)
